cbdf64efe3dfe67278510adc692451a0cd8dd378
thlo7777 <thlo7777@qq.com>
2022-12-04 16:31:27 +0800

    commit algorithm 4th exa

--- src/Main.java
+++ src/Main.java
@@ -2,61 +2,63 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    enum inputType {KeyNumber, KeyString, QuitGame, inValid}
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-        /* How to catch up an exception */
-        CatchupExeception catchupExeception = new CatchupExeception();
+  enum inputType {KeyNumber, KeyString, QuitGame, inValid}
 
-        Readable readable = new InputStreamReader(System.in);
-        Scanner scanner = new Scanner(readable);
-        while (isGameOver()) {
-            String inputStr = scanner.next();
-            System.out.println("display input string: " + inputStr);
-            if (checkInputKey(inputStr) == inputType.KeyNumber) {
-                System.out.println("continue");
-                int number = Integer.valueOf(inputStr);
-                if (number >= 0) {
-                    System.out.println("Positive number: " + Integer.valueOf(inputStr));
-                } else {
-                    System.out.println("Negtive number: " + Integer.valueOf(inputStr));
-                }
-                continue;
-            }
-            try {
-                catchupExeception.throwExeception();
-            } catch (IllegalStateException e) {
-                System.out.println(e.getMessage());
-                System.out.println("catch a IllegalStateException");
-                //e.printStackTrace();
-            } /*finally {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    /* How to catch up an exception */
+    CatchupExeception catchupExeception = new CatchupExeception();
+
+    Readable readable = new InputStreamReader(System.in);
+    Scanner scanner = new Scanner(readable);
+    while (isGameOver()) {
+      String inputStr = scanner.next();
+      System.out.println("display input string: " + inputStr);
+      if (checkInputKey(inputStr) == inputType.KeyNumber) {
+        System.out.println("continue");
+        int number = Integer.valueOf(inputStr);
+        if (number >= 0) {
+          System.out.println("Positive number: " + Integer.valueOf(inputStr));
+        } else {
+          System.out.println("Negtive number: " + Integer.valueOf(inputStr));
+        }
+        continue;
+      }
+      try {
+        catchupExeception.throwExeception();
+      } catch (IllegalStateException e) {
+        System.out.println(e.getMessage());
+        System.out.println("catch a IllegalStateException");
+        //e.printStackTrace();
+      } /*finally {
                 System.out.println("awlays execution these code");
             } */
 
-        }
     }
+  }
 
-    public static boolean isGameOver() {
-        System.out.println("check game if it is over");
-        return true;
+  public static boolean isGameOver() {
+    System.out.println("check game if it is over");
+    return true;
+  }
+
+  public static inputType checkInputKey(String str) {
+    if (str.equals("q") || str.equals("Q")) {
+      return inputType.QuitGame; //quit
     }
+    //Boolean strResult = str.matches("^[-+]?[0-9]+.?[0-9]*$"); //positive negtive number or +-float
+    //Boolean strResult = str.matches("^[0-9]*[1-9][0-9]*$"); //positive number
+    Boolean strResult = str.matches("^+?[1-9][0-9]*$"); //only non zero positive number
 
-    public static inputType checkInputKey(String str) {
-        if (str.equals("q") || str.equals("Q")) {
-            return inputType.QuitGame; //quit
-        }
-        //Boolean strResult = str.matches("^[-+]?[0-9]+.?[0-9]*$"); //positive negtive number or +-float
-        //Boolean strResult = str.matches("^[0-9]*[1-9][0-9]*$"); //positive number
-        Boolean strResult = str.matches("^+?[1-9][0-9]*$"); //only non zero positive number
-
-        if (strResult) {
-            return inputType.KeyNumber; //only non zero positive number
-        } else {
-            if (str.equals('q') || str.equals('Q')) {
-                return inputType.KeyString; //string
-            }
-        }
-        return inputType.inValid;
+    if (strResult) {
+      return inputType.KeyNumber; //only non zero positive number
+    } else {
+      if (str.equals('q') || str.equals('Q')) {
+        return inputType.KeyString; //string
+      }
     }
+    return inputType.inValid;
+  }
 }
---
+++ src/algorithms_4th/Exa1_1_1.java
@@ -0,0 +1,8 @@
+package algorithms_4th;
+
+public class Exa1_1_1 {
+  public static void main(String[] args) {
+    System.out.println("(0 + 15) / 2 = " + (0+15)/2);
+  }
+
+}
